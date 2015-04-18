3d25ece20f9228a928ea5f1403219a87bd6ebe8a
joanna20828 <joanna20828@gmail.com>
2015-04-19 02:14:58 +0800

    v3

--- pro1/Sudoku.cpp
+++ pro1/Sudoku.cpp
@@ -7,8 +7,8 @@
 void GiveQuestion()
 {
 	vector <int > board;
-
 	
+	int DigSudoku();
 
 	PrintBoard(board);	
 }
@@ -24,4 +24,35 @@ void PrintBoard(vector <int > board)
         	}
     	}
     cout << endl;
+}
+
+int DigSudoku()
+{
+	int d, ka, kb, kc; 
+	int Dig[Mid];   // 挖洞順序
+   
+	for(d=0; d<Mid; d++) Dig[d]=d;
+	for(d=0; d<Mid; d++)  // 打亂挖洞順序
+	{
+		int n = rand()%Mid;
+		int t = Dig[n];  Dig[n]=Dig[d]; Dig[d] = t;  //交換     
+	}    
+	for(d=0; d<Mid; d++)
+	{
+		Ans=0; kb = -1; kc = Max-Dig[d]-1;
+		ka = Board[Dig[d]];  Board[Dig[d]]=0;  // 挖洞
+		if (kc!=Dig[d])   // 對稱 
+			{
+				kb = Board[kc];  
+				Board[kc] = 0;
+			}
+		SolveSudoku(Board);
+		if (Ans>1)  // 有多重解, 挖洞需填回
+		{
+			Board[Dig[d]] = ka;
+			if (kb>0) Board[kc] = kb;
+      	}
+   }
+   }
+   return 0;
 }
